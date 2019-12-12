cc03f6acf214ab3773cfe0d575a3ede120b627ef
Sloane <sloanems@aol.com>
2019-12-12 00:24:01 +0000

    simple form

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -2,4 +2,8 @@ class NamesController < ApplicationController
   def index
     @names = Name.all
   end
+
+  def new
+    @name = Name.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'names#index'
+  resources :names
 end
