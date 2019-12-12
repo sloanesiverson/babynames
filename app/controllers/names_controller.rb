15a6246db7b76f2fe864337473761b93125eed92
Sloane <sloanems@aol.com>
2019-12-12 00:36:54 +0000

    nav links

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -6,4 +6,15 @@ class NamesController < ApplicationController
   def new
     @name = Name.new
   end
+
+  def create
+    Name.create(name_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def name_params
+    params.require(:name).permit(:name)
+  end
 end
